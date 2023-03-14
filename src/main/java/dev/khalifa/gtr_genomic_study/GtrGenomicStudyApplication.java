c8f5747a36a34f92819028115388037e2b5ad86c
khalifa-aly <alymokhalifa@gmail.com>
2023-03-14 14:42:27 -0500

    Initial commit

---
+++ src/main/java/dev/khalifa/gtr_genomic_study/GtrGenomicStudyApplication.java
@@ -0,0 +1,13 @@
+package dev.khalifa.gtr_genomic_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtrGenomicStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtrGenomicStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/khalifa/gtr_genomic_study/GtrGenomicStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.khalifa.gtr_genomic_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtrGenomicStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
