e0607485b93ca88c236cf6ebbc25680e383aaa6e
khalifa-aly <alymokhalifa@gmail.com>
2023-03-14 14:48:14 -0500

    renaming and some backbone packages

--- src/main/java/dev/khalifa/gtr_genomic_study/Application.java
+++ src/main/java/dev/khalifa/gtr_genomic_study/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GtrGenomicStudyApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(GtrGenomicStudyApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
