3652e1efaf99976c20511c62e664b478b19ba77c
khalifa-aly <alymokhalifa@gmail.com>
2023-03-14 17:53:35 -0500

    fixing column names to match underscore and spring data repository queries

--- src/main/java/dev/khalifa/gtrGenomicStudy/model/GtrEntry.java
+++ src/main/java/dev/khalifa/gtrGenomicStudy/model/GtrEntry.java
@@ -6,35 +6,63 @@ import org.springframework.data.relational.core.mapping.Column;
 public record GtrEntry (
         @Id
         Integer id,
-        String test_accession_ver ,
-        String name_of_laboratory ,
-        String name_of_institution ,
-        String facility_state ,
-        String facility_postcode ,
-        String facility_country ,
+        @Column(value = "test_accession_ver")
+        String testAccessionVer ,
+        @Column(value = "name_of_laboratory")
+        String nameOfLaboratory ,
+        @Column(value = "name_of_institution")
+        String nameOfInstitution ,
+        @Column(value = "facility_state")
+        String facilityState ,
+        @Column(value = "facility_postcode")
+        String facilityPostcode ,
+        @Column(value = "facility_country")
+        String facilityCountry ,
 //        @Column(value = "CLIA_number")
-        String clia_number,
-        String state_licenses,
-        String state_license_numbers,
-        String lab_test_id,
-        String last_touch_date,
-        String lab_test_name,
-        String manufacturer_test_name,
-        String test_development,
-        String lab_unique_code,
-        String condition_identifiers,
-        String indication_types,
+        @Column(value = "clia_number")
+        String cliaNumber,
+        @Column(value = "state_licenses")
+        String stateLicenses,
+        @Column(value = "state_license_numbers")
+        String stateLicenseNumbers,
+        @Column(value = "lab_test_id")
+        String labTestId,
+        @Column(value = "last_touch_date")
+        String lastTouchDate,
+        @Column(value = "lab_test_name")
+        String labTestName,
+        @Column(value = "manufacturer_test_name")
+        String manufacturerTestName,
+        @Column(value = "test_development")
+        String testDevelopment,
+        @Column(value = "lab_unique_code")
+        String labUniqueCode,
+        @Column(value = "condition_identifiers")
+        String conditionIdentifiers,
+        @Column(value = "indication_types")
+        String indicationTypes,
+
         String inheritances,
-        String method_categories,
+        @Column(value = "method_categories")
+        String methodCategories,
+
         String methods,
         String platforms,
         String genes,
-        String drug_responses,
-        String now_current,
-        String test_curr_stat,
-        String test_pub_stat,
-        String lab_curr_stat,
-        String lab_pub_stat,
-        String test_create_date,
-        String test_deletion_date
+        @Column(value = "drug_responses")
+        String drugResponses,
+        @Column(value = "now_current")
+        String nowCurrent,
+        @Column(value = "test_curr_stat")
+        String testCurrStat,
+        @Column(value = "test_pub_stat")
+        String testPubStat,
+        @Column(value = "lab_curr_stat")
+        String labCurrStat,
+        @Column(value = "lab_pub_stat")
+        String labPubStat,
+        @Column(value = "test_create_date")
+        String testCreateDate,
+        @Column(value = "test_deletion_date")
+        String testDeletionDate
 ){}
