b68a4385d65ff9b45106c6a4e9e47a03b8cadd61
khalifa-aly <alymokhalifa@gmail.com>
2023-03-14 14:54:00 -0500

    RestTemplate as web config

---
+++ src/main/java/dev/khalifa/gtr_genomic_study/config/WebConfig.java
@@ -0,0 +1,15 @@
+package dev.khalifa.gtr_genomic_study.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplateBuilder().build();
+    }
+}
+
