2f6452a045a901e2b59c2c832b7ef73eaaf69bef
khalifa-aly <alymokhalifa@gmail.com>
2023-03-15 13:00:29 -0500

    Getresponse json from a json file for easier editing later

--- src/main/java/dev/khalifa/gtrGenomicStudy/config/SpringDocConfig.java
+++ src/main/java/dev/khalifa/gtrGenomicStudy/config/SpringDocConfig.java
@@ -1,5 +1,6 @@
 package dev.khalifa.gtrGenomicStudy.config;
 
+import dev.khalifa.gtrGenomicStudy.util.ReadJsonFileToJsonObject;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.Components;
@@ -12,17 +13,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 
+import java.io.IOException;
+
 @Configuration
 @OpenAPIDefinition
 public class SpringDocConfig {
     @Bean
-    public OpenAPI baseOpenAPI(){
+    public OpenAPI baseOpenAPI() throws IOException {
+        ReadJsonFileToJsonObject readJsonFileToJsonObject = new ReadJsonFileToJsonObject();
         ApiResponse badRequestAPI = new ApiResponse().content(
                 new Content().addMediaType(MediaType.APPLICATION_JSON_VALUE,
                         new io.swagger.v3.oas.models.media.MediaType().addExamples(
                                 "default",
                                 new Example().value(
-                                        "{\"code\":400, \"Status\":\"Bad Request!\", \"Message\":\"Bad Request!\"}"
+//                                        "{\"code\":400, \"Status\":\"Bad Request!\", \"Message\":\"Bad Request!\"}"
+                                        readJsonFileToJsonObject.read().get("badRequestResponse").toString()
                                 )
                         ))
         ).description("Bad Request!");
@@ -32,7 +37,8 @@ public class SpringDocConfig {
                         new io.swagger.v3.oas.models.media.MediaType().addExamples(
                                 "default",
                                 new Example().value(
-                                        "{\"code\":500, \"Status\":\"Internal Server Error!\", \"Message\":\"Internal Server Error!\"}"
+//                                        "{\"code\":500, \"Status\":\"Internal Server Error!\", \"Message\":\"Internal Server Error!\"}"
+                                        readJsonFileToJsonObject.read().get("internalServerErrorResponse").toString()
                                 )
                         ))
         ).description("Internal Server Error!");
