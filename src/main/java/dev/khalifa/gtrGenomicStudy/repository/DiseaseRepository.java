2f317402f6d2733dc7be720ba2a2e103cb40b494
khalifa-aly <alymokhalifa@gmail.com>
2023-03-17 18:36:34 -0500

    disease repository

---
+++ src/main/java/dev/khalifa/gtrGenomicStudy/repository/DiseaseRepository.java
@@ -0,0 +1,11 @@
+package dev.khalifa.gtrGenomicStudy.repository;
+
+import dev.khalifa.gtrGenomicStudy.model.Disease;
+import dev.khalifa.gtrGenomicStudy.model.GtrEntry;
+import org.springframework.data.repository.ListCrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DiseaseRepository extends ListCrudRepository<Disease, Integer> {
+}
