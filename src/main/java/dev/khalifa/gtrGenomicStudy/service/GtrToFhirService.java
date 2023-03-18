175bb8903ca61e289f1a888f2b25f093389e76e5
khalifa-aly <alymokhalifa@gmail.com>
2023-03-17 21:59:07 -0500

    QA text

--- src/main/java/dev/khalifa/gtrGenomicStudy/service/GtrToFhirService.java
+++ src/main/java/dev/khalifa/gtrGenomicStudy/service/GtrToFhirService.java
@@ -170,7 +170,7 @@ public class GtrToFhirService {
 //        Referencing some example FHIR resources
         genomicStudy.setReferrer(new Reference("https://build.fhir.org/practitioner-example-f007-sh.html"));
         genomicStudy.setEncounter(new Reference("https://build.fhir.org/encounter-example.html"));
-        genomicStudy.setDescription("Test for"
+        genomicStudy.setDescription("Test for "
                 + gtrEntry.get().labTestName()
                 + " is available at: https://www.ncbi.nlm.nih.gov/gtr/tests/"
                 + gtrEntry.get().labTestId());
